73288c0e6795673a19ca40b3d85f06cf3419a80a
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-05 16:33:03 +0800

    上机习题

--- src/Sum.java
+++ src/Sum.java
@@ -1,25 +1,39 @@
 public class Sum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int i = 1;
         int sum = 0;
-        while (i<=100){
-            sum = sum+i;
+        while (i <= 100) {
+            sum = sum + i;
             i += 2;
         }
-        System.out.println("1-100之内的奇数和是"+ sum);
+        System.out.println("1-100之内的奇数和是" + sum);
     }
 }
-class OddSum{
-    public static void main(String[] args){
+
+class OddSum {
+    public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0;i<=100;i+=2){
-            sum = sum+i;
+        for (int i = 0; i <= 100; i += 2) {
+            sum = sum + i;
         }
-        System.out.println("1-100之内的偶数和是"+sum);
+        System.out.println("1-100之内的偶数和是" + sum);
     }
 }
-class divide{
-    public static void main(String[] args){
-        
+
+class Divide {
+    public static void main(String[] args) {
+        int i = 1;
+        int count = 0;
+        while (i <= 1000) {
+            if (i % 5 == 0) {
+                System.out.print(i + "\t");
+                count++;
+                if (count % 3 == 0) {
+                    System.out.println();
+                }
+            }
+            i++;
+        }
     }
 }
+//todo 第六期 96-100
